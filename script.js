331dfef831e5a776ce54e30bbb55376004079781
Glitch (hello-webpage) <none>
2020-11-16 21:47:12 +0000

    🏄🎯 Checkpoint ./index.html:22062302/35414 ./mobile.css:22062302/5204 ./script.js:22062302/292

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
