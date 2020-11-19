43e774825a61d827ee00ef0fc525dbe0ebf6aab7
Glitch (hello-webpage) <none>
2020-11-19 20:51:20 +0000

    😚🐝 Checkpoint ./script.js:22062302/40

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ $(function() {
         email: true
       }
     },
-    highlight: function(input, errorClass) {
-      $(input).fadeOut(function() {
-        $(input).fadeIn();
+    highlight: function(element, errorClass) {
+      $(element).fadeOut(function() {
+        $(element).fadeIn();
       });
     },
     submitHandler: function(form) {
