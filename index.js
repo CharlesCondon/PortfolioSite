6d2e581374664c6e2850f05ca04b5213297624dd
Glitch (hello-webpage) <none>
2020-11-20 00:41:18 +0000

    🌧😚 Checkpoint ./index.js:22062302/123 ./index.html:22062302/66 ./desktopXL.css:22062302/71

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $(".burn").hover(function() {
+    $(this).removeClass("burn");
     $(this).css("opacity", "1");
+  }, function(){
+    $(this).addClass("burn");
   });
 });
