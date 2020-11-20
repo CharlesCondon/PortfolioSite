2411cab54d5435ba5563799776d44a83dcc112e4
Glitch (hello-webpage) <none>
2020-11-20 22:59:21 +0000

    🚵🐆 Checkpoint ./validate.js:22062302/220 ./contact.html:22062302/40

--- validate.js
+++ validate.js
@@ -5,8 +5,16 @@
 console.log("hi");
 
 $.validator.setDefaults({
+  errorClass: 'help-block',
   highlight: function(element) {
     $(element)
+      .closest('.form-group')
+      .addClass('has-error');
+  },
+  unhighlight: function(element) {
+    $(element)
+      .closest('.form-group')
+      .removeClass('has-error');
   }
 });
 
