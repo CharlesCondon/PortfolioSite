0c3f9a85c44b6ac4951d1c465b74c1b23e200302
Charles <49871679+CharlesCondon@users.noreply.github.com>
2020-12-03 18:41:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// functions to make interactive flame
+// (does not work if homepage is left and then returned 
+// to unless user refreshes page)
 $(document).ready(function() {
-  $(".burn").hover(function() {
-    $(this).removeClass("burn");
-    $(this).css("opacity", "1");
-  }, function(){
-    $(this).addClass("burn");
-  });
+  $(".burn").hover(
+    function() {
+      $(this).removeClass("burn");
+      $(this).css("opacity", "1");
+    },
+    function() {
+      $(this).addClass("burn");
+    }
+  );
 });
